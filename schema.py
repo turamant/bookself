ed858ac131b87abc76a594d14c541da7f2a999f6
turamant <tur1amant@gmail.com>
2024-08-01 16:05:00 +0300

    schema

---
+++ schema.py
@@ -0,0 +1,43 @@
+import graphene
+from models import Book as BookModel
+
+class BookType(graphene.ObjectType):
+    id = graphene.String()
+    title = graphene.String()
+    category = graphene.String()
+    author = graphene.String()
+    cover = graphene.String()
+    price = graphene.Float()
+    poster = graphene.String()
+    year = graphene.Int()
+    count = graphene.Int()
+
+class Query(graphene.ObjectType):
+    all_books = graphene.List(BookType)
+
+    def resolve_all_books(self, info):
+        books = BookModel.get_all()
+        return [BookType(**book) for book in books]
+
+class CreateBook(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        category = graphene.String(required=True)
+        author = graphene.String(required=True)
+        cover = graphene.String(required=True)
+        price = graphene.Float(required=True)
+        poster = graphene.String(required=True)
+        year = graphene.Int(required=True)
+        count = graphene.Int(required=True)
+
+    book = graphene.Field(BookType)
+
+    def mutate(self, info, title, category, author, cover, price, poster, year, count):
+        book = BookModel(title, category, author, cover, price, poster, year, count)
+        book.save()
+        return CreateBook(book=BookType(**book.__dict__))
+
+class Mutation(graphene.ObjectType):
+    create_book = CreateBook.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
